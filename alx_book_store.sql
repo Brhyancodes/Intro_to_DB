288e07f5c7f81c371ea5155163a3eb2cc7fe6b24
Brian Wakhale <brianweke1998@gmail.com>
2025-08-15 14:02:26 +0300

    Fixed minor bug

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,8 +1,5 @@
--- Create the database if it doesn't exist
 CREATE DATABASE
 IF NOT EXISTS alx_book_store;
-
--- Use the database
 USE alx_book_store;
 
 -- Authors: Stores information about authors.
